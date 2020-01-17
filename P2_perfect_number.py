8f2f6d9271c314c7fad16efe357727012bd6fa4a
Hongwenhui-hwh <57311609+Hongwenhui-hwh@users.noreply.github.com>
2020-01-17 20:54:24 +0800

    Update P2_perfect_number.py

--- P2_perfect_number.py
+++ P2_perfect_number.py
@@ -2,7 +2,7 @@ i = 1
 for i in range(1, 10000):
     j = 1
     x = 0
-    for j in range(1, 10000):
+    for j in range(1, i):
         if (j!=i)&(i%j==0):
             x = x + j
             if x==i:
