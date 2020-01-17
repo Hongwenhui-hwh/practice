5c8309a85dc97f3939b81663cf42f221975fd183
Hongwenhui-hwh <57311609+Hongwenhui-hwh@users.noreply.github.com>
2020-01-17 21:28:16 +0800

    Update P3_fibonacci_sequence.py

--- P3_fibonacci_sequence.py
+++ P3_fibonacci_sequence.py
@@ -1,8 +1,13 @@
 #     Generate the first 20 numbers of the Fibonacci sequence
-fbnq = [0 for i in range(20)]
-for i in range(0,20):
-    if i<2:
-       fbnq[i] = 1
-    else:
-       fbnq[i] = fbnq[i-1] + fbnq[i-2]
-       print(fbnq[i])
+def fbnq_se():
+    fbnq = [0 for i in range(20)]
+    for i in range(0,20):
+        if i<2:
+            fbnq[i] = 1
+        else:
+            fbnq[i] = fbnq[i-1] + fbnq[i-2]
+            print(fbnq[i])
+    return 
+
+if __name__ == '__main__':
+    fbnq_se()
